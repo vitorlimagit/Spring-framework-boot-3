5d4e2072cdc02d37848fa46e323f6d08ea53e4b8
VitorLima <vitorgl@ciandt.com>
2023-02-26 18:29:14 -0300

    Implementing Auto Wiring in Spring Framework

--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person (String name, int age){ };
+record Person (String name, int age, Address address){ };
 
 record Address(String firstLine, String city){ };
 
@@ -12,7 +12,7 @@ public class HelloWorldConfiguration {
 
     @Bean
     public String name(){
-        return "Vitor";
+        return "Ranga";
     }
     @Bean
     public int age(){
@@ -21,11 +21,28 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person(){
-        return new Person("Lima", 20);
+        return new Person("Ravi", 20,new Address("Main Street", "Utrecht"));
     }
+
     @Bean
+    public Person person2MethodCall(){
+        return new Person(name(), age(), address()); // name, age
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3){
+        //name,age,address2
+        return new Person(name, age, address3); // name, age
+    }
+
+    @Bean(name = "address2")
     public Address address(){
         return new Address("Baker Street", "London");
+    }
+
+    @Bean(name = "address3")
+    public Address address3(){
+        return new Address("Motinaga", "Hyderabad");
     }
 
 }

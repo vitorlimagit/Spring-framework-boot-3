9e9d7cac551c16cf8c181e9e2f3ce92b3f848e83
VitorLima <vitorgl@ciandt.com>
2023-02-26 18:04:59 -0300

    First Managed bean by Spring

--- src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
+++ src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
@@ -1,18 +1,17 @@
 package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
-import com.in28minutes.learnspringframework.game.MarioGame;
 import com.in28minutes.learnspringframework.game.PacmanGame;
-import com.in28minutes.learnspringframework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 
     public static void main(String[] args) {
 
        // var game = new MarioGame();
        // var game = new SuperContraGame();
-        var game = new PacmanGame();
-        var gameRunner = new GameRunner(game);
+        var game = new PacmanGame(); // 1: Object Creation
+        var gameRunner = new GameRunner(game); // 2: Object Creation + Wiring of Dependencies
+        // Game is a Dependecy of GameRunner
         gameRunner.run();
     }
 }
---
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -0,0 +1,23 @@
+package com.in28minutes.learnspringframework;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.PacmanGame;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App02HelloWorldSpring {
+
+    public static void main(String[] args) {
+        //1: Launch a Spring context
+        AnnotationConfigApplicationContext
+                context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
+        //2: configure the things that we want Spring to manage -
+        // HelloWorldConfiguration - @Configuration
+        // name - @Bean
+
+        //3: Retrieving Beans managed by Spring
+        System.out.println(
+        context.getBean("name"));
+
+    }
+}

093dd40e694a6593c28cb85ba3b0490fee9849fb
VitorLima <vitorgl@ciandt.com>
2023-02-26 18:13:57 -0300

    Crating more java beans with record

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -16,8 +16,12 @@ public class App02HelloWorldSpring {
         // name - @Bean
 
         //3: Retrieving Beans managed by Spring
-        System.out.println(
-        context.getBean("name"));
+        System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+
+        System.out.println(context.getBean("person"));
+
+        System.out.println(context.getBean("address"));
 
     }
 }
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,12 +3,29 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age){ };
+
+record Address(String firstLine, String city){ };
+
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name(){
         return "Vitor";
+    }
+    @Bean
+    public int age(){
+        return 15;
+    }
+
+    @Bean
+    public Person person(){
+        return new Person("Lima", 20);
+    }
+    @Bean
+    public Address address(){
+        return new Address("Baker Street", "London");
     }
 
 }

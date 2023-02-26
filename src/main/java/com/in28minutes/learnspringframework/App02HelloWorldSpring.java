9d5de2a22b250987454ddd8c82f75c2bf1cf06e1
VitorLima <vitorgl@ciandt.com>
2023-02-26 19:46:42 -0300

    Exploring Framework Bean Auto Wiring - Primary and Qualifier

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -4,6 +4,9 @@ import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.PacmanGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class App02HelloWorldSpring {
 
     public static void main(String[] args) {
@@ -27,7 +30,16 @@ public class App02HelloWorldSpring {
 
         System.out.println(context.getBean("address2"));
 
-        //System.out.println(context.getBean(Address.class));
+        System.out.println(context.getBean("person5Qualifier"));
+
+
+        System.out.println(context.getBean(Person.class));
+
+        System.out.println(context.getBean(Address.class));
+
+//        Arrays.stream(context.getBeanDefinitionNames())
+//                .forEach(System.out::println);
+
 
     }
 }
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
 package com.in28minutes.learnspringframework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 record Person (String name, int age, Address address){ };
 
@@ -35,12 +37,27 @@ public class HelloWorldConfiguration {
         return new Person(name, age, address3); // name, age
     }
 
+    @Bean
+    @Primary
+    public Person person4Parameters(String name, int age, @Qualifier("address3qualifier") Address address3){
+        //name,age,address2
+        return new Person(name, age, address3); // name, age
+    }
+
+    @Bean
+    public Person person5Qualifier(String name, int age, Address address){
+        //name,age,address2
+        return new Person(name, age, address); // name, age
+    }
+
     @Bean(name = "address2")
+    @Primary
     public Address address(){
         return new Address("Baker Street", "London");
     }
 
     @Bean(name = "address3")
+    @Qualifier("address3qualifier")
     public Address address3(){
         return new Address("Motinaga", "Hyderabad");
     }
